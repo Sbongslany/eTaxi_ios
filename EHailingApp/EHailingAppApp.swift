3ca6ebb8057c15603d06d69dfc1fe24eb732d1ae
Sibongiseni Ndebele <43204005+Sbongslany@users.noreply.github.com>
2026-03-25 19:21:15 +0200

    Initial Commit

---
+++ EHailingApp/EHailingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EHailingAppApp.swift
+//  EHailingApp
+//
+//  Created by Xolani Ndebele on 25/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct EHailingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
